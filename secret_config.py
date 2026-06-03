fa214cb3adf291563dd68b902d0a865de52480b9
Juan Idarraga <jidarraga721@gmail.com>
2026-06-03 02:18:00 -0500

    Sube secret_config

---
+++ secret_config.py
@@ -0,0 +1,5 @@
+PGHOST='dpg-d826ed67r5hc73e6oep0-a.ohio-postgres.render.com'
+PGDATABASE='liquidador_de_nomina'
+PGUSER='liquidador_de_nomina_user'
+PGPASSWORD='5aUKHCaJYA96Qks9kvHrir5hidoMvZLm'
+PGPORT=5432
