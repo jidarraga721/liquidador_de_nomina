ba3ad9b487439ad9efc4376045494b8f70b9b26a
Juan Idarraga <jidarraga721@gmail.com>
2026-06-03 00:05:09 -0500

    CRUD completo y pruebas unitarias funcionando

--- app.py
+++ app.py
@@ -1,53 +1,29 @@
-from flask import Flask,render_template, request
-
+from flask import Flask
 
 from src.routes.empleados_routes import empleados_bp
 from src.routes.nomina_routes import nomina_bp
 from src.routes.detalle_routes import detalle_bp
+
 from src.controller.empleados_controller import EmpleadosController
 from src.controller.nomina_controller import NominaController
 from src.controller.detalle_nomina_controller import DetalleNominaController
+
 app = Flask(__name__)
 
 app.register_blueprint(empleados_bp)
 app.register_blueprint(nomina_bp)
 app.register_blueprint(detalle_bp)
 
+
 @app.route('/crear_tablas')
 def crear_tablas():
 
     EmpleadosController.crear_tabla()
+    NominaController.crear_tabla()
+    DetalleNominaController.crear_tabla()
 
     return "Tablas creadas correctamente"
 
-
-
-    empleado = None
-
-    if request.method == 'POST':
-
-        cedula = request.form['cedula']
-
-        empleado = EmpleadosController.buscar_empleado(cedula)
-
-    return render_template(
-        'buscar_empleado.html',
-        empleado=empleado
-    )
-
-
-
-    if request.method == 'POST':
-
-        nombre = request.form['nombre']
-        cedula = request.form['cedula']
-        cargo = request.form['cargo']
-        salario_base = request.form['salario_base']
-        fecha_ingreso = request.form['fecha_ingreso']
-
-        return f"Empleado {nombre} guardado correctamente"
-
-    return render_template('insertar_empleado.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
